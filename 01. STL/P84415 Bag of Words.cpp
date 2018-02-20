dd0d340a190d31e31eb46c46f8ed91b21f7b4e34
Júlia Gasull <juliagasull@icloud.com>
2018-02-20 17:55:49 +0100

    Update P84415 Bag of Words.cpp

--- 01. STL/P84415 Bag of Words.cpp
+++ 01. STL/P84415 Bag of Words.cpp
@@ -5,45 +5,59 @@
 //  Created by Roberto Ariosa Hernández on 19/09/2017.
 //  Copyright © 2017 Mr.Robb. All rights reserved.
 //
-/*
-#include <iostream>
-#include <queue>
 
+#include <iostream>
+#include <map>
+#include <string>
 using namespace std;
 
-void	print_queue(priority_queue<pair<string, int>, vector<pair<string, int>>, greater<int>>& q) {
-	while (not q.empty()) {
-		cout << q.top().first << ' ' << q.top().second << endl;
-		q.pop();
-	}
+//store p
+//delete p
+//maximum?
+//minimum?
+
+
+int main()
+{
+    map <string, int> m;
+    string op;
+    string x;
+    
+    while (cin >> op)
+    {
+        if (op == "store")
+        {
+            cin >> x;
+            if (m.find(x) != m.end()) ++m[x];
+            else m[x] = 1;
+        }
+        else if (op == "delete")
+        {
+            cin >> x;
+            if (m.find(x) != m.end())
+            {
+                if (m[x] > 1) --m[x];
+                else m.erase(x);
+            }
+        }
+        else if (op == "minimum?")
+        {
+            if (m.empty()) cout << "indefinite minimum" << endl;
+            else
+            {
+                map<string, int>::iterator it= m.begin();
+                cout << "minimum: " << it->first << ", " << it->second << " time(s)" << endl;
+            }
+        }
+        else //maximum?
+        {
+            if (m.empty()) cout << "indefinite maximum" << endl;
+            else
+            {
+                map<string, int>::iterator it = m.end();
+                --it;
+                cout << "maximum:" << " " << it->first << ", " << it->second << " time(s)" << endl;
+            }
+        }
+    }
 }
-
-
-
-int main() {
-	string op;
-	priority_queue<pair<string, int>, vector<pair<string, int>>, greater<int>> q;
-	pair<string, int> p;
-	p.first = "hello";
-	p.second = 1;
-	q.push(p);
-	q.push(p);
-	p.second = 2;
-	q.push(p);
-	print_queue(q);
-//	while (cin >> op) {
-//		if (op == "minimum?") {
-//
-//		}
-//		else if (op == "maximum?") {
-//
-//		}
-//		else if (op == "delete") {
-//
-//		}
-//		else if (op == "store") {
-//
-//		}
-//	}
-}
-*/
